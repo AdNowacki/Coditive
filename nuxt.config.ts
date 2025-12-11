3eb9f1fbcc79f5a6c8069ea3b396ea3fbc2a6d23
Adam Nowacki <ad.nowacki@gmail.com>
2025-12-11 11:18:39 +0100

    Initial project

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+import tailwindcss from "@tailwindcss/vite"
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxt/content', '@nuxt/eslint'],
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: '2024-04-03',
+  css: ['./app/assets/styles/main.scss'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  }
+})
